1f760a5ae966230cd6a7edd75e601029525aafd3
pawelek7k <144838381+pawelek7k@users.noreply.github.com>
2024-02-10 14:19:43 +0000

    Deploying to gh-pages from @ pawelek7k/animeCartoon@4c2b2d1e41402650ea4dd7f474778c36d3a3c5f7 🚀

---
+++ index.1c466f6d.js
@@ -0,0 +1,2 @@
+var hamburgerHtml=document.querySelector(".menu-svg-header"),navMenuHtml=document.querySelector(".navigation-list");hamburgerHtml.addEventListener("click",(function(){hamburgerHtml.classList.toggle("active"),navMenuHtml.classList.toggle("active")})),document.querySelectorAll;
+//# sourceMappingURL=index.1c466f6d.js.map
