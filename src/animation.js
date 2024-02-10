32c3c9d19487784f81596978ccbe5f8a41e2f206
pawelek <k.pawelek@vp.pl>
2024-02-10 14:23:12 +0100

    animation-js

---
+++ src/animation.js
@@ -0,0 +1,24 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElementsHtml = document.querySelectorAll('.hidden');
+
+hiddenElementsHtml.forEach(element => {
+  observer.observe(element);
+});
+
+const linksHtml = document.querySelectorAll('a');
+
+linksHtml.forEach(link => {
+  link.addEventListener('click', () => {
+    event.preventDefault();
+  });
+});
