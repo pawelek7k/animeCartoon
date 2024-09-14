55751cadb3fdafc99b3ac7486567b631ac449d02
pawelek <k.pawelek@vp.pl>
2024-09-14 14:15:26 +0200

    add hero landing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import LandingHeader from './components/LandingHeader.vue'
+import LandingHero from './components/LandingHero.vue'
 </script>
 
 <template>
   <LandingHeader />
-
+  <LandingHero />
   <main></main>
 </template>
 
---
+++ src/components/LandingHero.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <m-input placeholder="Search..." />
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
