f478fa4eedab8658344bbdfff5c6b9cb93319fbb
pawelek <k.pawelek@vp.pl>
2024-09-16 19:55:24 +0200

    add router dynamic

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import AnimeDetails from '../views/AnimeDetails.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         name: 'About',
         component: About,
     },
+    {
+        path: '/anime/:id',
+        name: 'Anime Details',
+        component: AnimeDetails
+    }
 ];
 
 const router = createRouter({
