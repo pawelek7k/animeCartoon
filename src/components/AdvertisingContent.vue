398d71a2967499c082a82e5ae973f8e4479276c5
pawelek <k.pawelek@vp.pl>
2024-09-27 17:00:50 +0200

    Update AdvertisingContent.vue

--- src/components/AdvertisingContent.vue
+++ src/components/AdvertisingContent.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PrimaryButton from './PrimaryButton.vue'
+</script>
 
 <template>
   <div class="ad-container">
@@ -6,18 +8,29 @@
       <h2>Watch this series without ads!</h2>
       <p>Try AnimeCartoon Premium free for 7 days.</p>
     </div>
-    <button>Try premium!</button>
+    <PrimaryButton label="Try premium!" />
   </div>
 </template>
 
 <style scoped>
 .ad-container {
-  width: 500px;
-
-  border: thin solid red;
-  background: url('/public/images/ad.jfif');
+  width: 700px;
+  display: flex;
+  justify-content: space-around;
+  padding: 2rem;
+  border-radius: 2rem;
+  background: linear-gradient(90deg, rgba(0, 0, 0, 0.7315301120448179) 0%, rgba(0, 0, 0, 1) 100%),
+    url('/public/images/ad.jfif');
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+}
+
+h2 {
+  font-family: 'Trade Winds', system-ui;
+}
+
+p {
+  font-size: 0.9rem;
 }
 </style>
