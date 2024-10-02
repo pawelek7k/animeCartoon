01bcff53f49713c8b8f36d80843e7a253ed44318
pawelek <k.pawelek@vp.pl>
2024-10-02 15:23:25 +0200

    add fixes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import DataFilter from './components/DataFilter.vue'
 import LandingFooter from './components/LandingFooter.vue'
 import MainNavigation from './components/MainNavigation.vue'
 </script>
 
 <template>
   <MainNavigation />
+  <DataFilter />
   <LandingFooter />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import FirstAnimeCover from '@/components/FirstAnimeCover.vue'
-import DataFilter from '../components/DataFilter.vue'
 import FetchingNewAnime from '../components/FetchingNewAnime.vue'
 import FetchingPopularAnime from '../components/FetchingPopularAnime.vue'
 import LandingHero from '../components/LandingHero.vue'
 </script>
 
 <template>
-  <DataFilter />
   <main>
     <LandingHero />
     <FetchingPopularAnime />
