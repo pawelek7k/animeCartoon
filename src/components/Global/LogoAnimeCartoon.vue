0fa042f53d265846cf728b78c82b49441b6cdf63
pawelek <k.pawelek@vp.pl>
2024-10-12 12:41:34 +0200

    Update LogoAnimeCartoon.vue

--- src/components/Global/LogoAnimeCartoon.vue
+++ src/components/Global/LogoAnimeCartoon.vue
@@ -2,8 +2,26 @@
 
 <template>
   <div>
-    <h1>Anime<span>Cartoon</span></h1>
+    <h1>
+      <router-link to="/" class="nav-link">Anime<span>Cartoon</span></router-link>
+    </h1>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-family: 'Trade Winds', system-ui;
+  cursor: pointer;
+}
+
+span {
+  background: -webkit-linear-gradient(var(--primary), var(--secondary));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: var(--text);
+}
+</style>
