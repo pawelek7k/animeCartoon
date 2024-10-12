6b6c7a2872c4bd7c3b06724c27e5194c20672b10
pawelek <k.pawelek@vp.pl>
2024-10-12 12:56:27 +0200

    Update LoaderSpinner.vue

--- src/components/Global/LoaderSpinner.vue
+++ src/components/Global/LoaderSpinner.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="loader-container">
-    <span class="loader"></span>
+  <div class="overlay">
+    <div class="loader-container">
+      <span class="loader"></span>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(5px);
+  z-index: 998;
+}
+
 .loader-container {
   position: fixed;
   top: 50%;
   z-index: 999;
   left: 50%;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   transform: translate(-50%, -50%);
+  -webkit-box-shadow: 0px 0px 100px 34px rgba(227, 162, 148, 1);
+  -moz-box-shadow: 0px 0px 100px 34px rgba(227, 162, 148, 1);
+  box-shadow: 0px 0px 100px 34px rgba(227, 162, 148, 1);
 }
 
 .loader {
